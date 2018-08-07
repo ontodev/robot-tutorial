179113c2e21a466f2bb519d6d649c802baa0d1e0
Chris Mungall <cjm@berkeleybop.org>
2018-08-07 09:25:18 -0700

    Create docker.sh
    Note odkfull has robot 1.1.0 currently, do you need 1.20-alpha?

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# run this by typing:
+#
+# sh docker.sh
+#
+# This will place you in a bash shell, in this folder
+# robot will be in your path, 
+# all commands from the tutorial can be executed in this environment
+#
+docker run -v $PWD:/work -w /work --rm -ti obolibrary/odkfull bash
