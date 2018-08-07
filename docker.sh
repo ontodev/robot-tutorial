15ec7576173fe2e474fd4b68159392c20c5b6a4c
Chris Mungall <cjm@berkeleybop.org>
2018-08-07 10:56:40 -0700

    Update docker.sh
    use lightweight robot container

--- docker.sh
+++ docker.sh
@@ -8,4 +8,5 @@
 # robot will be in your path, 
 # all commands from the tutorial can be executed in this environment
 #
-docker run -v $PWD:/work -w /work --rm -ti obolibrary/odkfull bash
+#docker run -v $PWD:/work -w /work --rm -ti obolibrary/odkfull bash
+docker run -v $PWD:/work -w /work --rm -ti obolibrary/robot:v1.2.0-alpha bash
